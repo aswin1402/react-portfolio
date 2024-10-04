1d119688d68b81a3ceb1801503ba336ba5710198
aswin vijayan <aswinvijayan12b@gmail.com>
2024-10-04 09:36:10 +0530

    react-portfolio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Contact from "./components/Contact";
+
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Skills/>
+      <Portfolio/>
+     
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
